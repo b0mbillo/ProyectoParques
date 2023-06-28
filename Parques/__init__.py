3d4f5738428868a3b7b24bc6ca55c6dd2d6c1ea6
Brando Montoya <bmontoyaj@unal.edu.co>
2023-06-28 16:35:55 -0500

    Delete Parques directory

--- Parques/__init__.py
+++
@@ -1,7 +0,0 @@
-#__all__ = ["Carcel","Casilla","Cielo","Constantes","Dado","Ficha","Fondo","Jugador","Tablero","Turno"]
-
-from .datos import *
-from .tablero import Tablero
-from .ficha import Ficha
-from .dado import Dado
-from .interfaz_configuracion import InterfazConfig
